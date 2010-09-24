da3222f3e4af5f6c1a79aadef031e149943eb032
Marek Majkowski <marek@popcount.org>
2010-09-24 14:50:20 +0000

    Python tutorial

---
+++ python/receive.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.AsyncoreConnection(pika.ConnectionParameters(
+        host='127.0.0.1',
+        credentials=pika.PlainCredentials('guest', 'guest')))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='test')
+
+def callback(ch, method, header, body):
+    print " [x] Received %.20r" % (body,)
+
+channel.basic_consume(callback,
+                      queue='test',
+                      no_ack=True)
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+pika.asyncore_loop()
---
+++ python/send.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.AsyncoreConnection(pika.ConnectionParameters(
+        host='127.0.0.1',
+        credentials=pika.PlainCredentials('guest', 'guest')))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='test')
+
+channel.basic_publish(exchange='',
+                      routing_key='test',
+                      body='Hello World!')
+print " [x] Sent 'Hello World!'"
+
