0e3a56927164d14643fa9975696245fab520e767
Marek Majkowski <majek04@gmail.com>
2010-11-23 13:18:07 +0000

    Minor tweaks to first example code, queue should be autodelete=false etc

--- php/receive.php
+++ php/receive.php
@@ -1,20 +1,20 @@
 <?php
 
 require_once(__DIR__ . '/lib/php-amqplib/amqp.inc');
-include_once(__DIR__ . '/config/config.php');
 
-$connection = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('test');
+
+$channel->queue_declare('test', false, false, false, false);
 
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
-$callback = function($msg){
+$callback = function($msg) {
   echo " [x] Received ", $msg->body, "\n";
 };
 
-$channel->basic_consume('test', 'consumer_tag', false, true, false, false, $callback);
+$channel->basic_consume('test', '', false, true, false, false, $callback);
 
 while(count($channel->callbacks)) {
     $channel->wait();
