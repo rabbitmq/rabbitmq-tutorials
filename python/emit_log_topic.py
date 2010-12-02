4eb4b9a2ec3ee115b8cde16ffec05e24f76723aa
Marek Majkowski <majek04@gmail.com>
2010-12-02 11:56:49 +0000

    Topic routing examples.

---
+++ python/emit_log_topic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.AsyncoreConnection(pika.ConnectionParameters(
+        host='127.0.0.1',
+        credentials=pika.PlainCredentials('guest', 'guest')))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs',
+                         type='topic')
+
+routing_key = sys.argv[1] if len(sys.argv) > 1 else 'anonymous.info'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
+channel.basic_publish(exchange='topic_logs',
+                      routing_key=routing_key,
+                      body=message)
+print " [x] Sent %r:%r" % (routing_key, message)
+connection.close()
---
+++ python/receive_logs_topic.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.AsyncoreConnection(pika.ConnectionParameters(
+        host='127.0.0.1',
+        credentials=pika.PlainCredentials('guest', 'guest')))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs',
+                         type='topic')
+
+result = channel.queue_declare(auto_delete=True)
+queue_name = result.queue
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    print >> sys.stderr, "Usage: %s [binding_key]..." % (sys.argv[0],)
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(exchange='topic_logs',
+                       queue=queue_name,
+                       routing_key=binding_key)
+
+print ' [*] Waiting for logs. To exit press CTRL+C'
+
+def callback(ch, method, header, body):
+    print " [x] %r:%r" % (method.routing_key, body,)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+pika.asyncore_loop()
