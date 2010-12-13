0cd19cb060ee9d14928849fcf62f41278d93bf06
Marek Majkowski <majek04@gmail.com>
2010-12-13 14:35:02 +0000

    Don't handle exceptions in java example

--- java/Recv.java
+++ java/Recv.java
@@ -4,31 +4,23 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.QueueingConsumer;
 
 public class Recv {
-  public static void main(String[] argv) {
-    try {
-      Connection conn = null;
-      try {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        conn = factory.newConnection();
-        Channel chan = conn.createChannel();
-        chan.queueDeclare("hello", false, false, false, null);
+    public static void main(String[] argv)
+        throws java.io.IOException,
+               java.lang.InterruptedException {
+    Connection conn = null;
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    conn = factory.newConnection();
+    Channel chan = conn.createChannel();
 
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-        QueueingConsumer consumer = new QueueingConsumer(chan);
-        chan.basicConsume("hello", true, consumer);
-        while (true) {
-          QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-          System.out.println(" [x] Received " + new String(delivery.getBody()));
-        }
-      }
-      finally {
-        if (conn != null) conn.close();
-      }
-    }
-    catch (Exception e) {
-      System.err.println("Exception while consuming");
-      e.printStackTrace();
+    chan.queueDeclare("hello", false, false, false, null);
+
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    QueueingConsumer consumer = new QueueingConsumer(chan);
+    chan.basicConsume("hello", true, consumer);
+    while (true) {
+      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+      System.out.println(" [x] Received " + new String(delivery.getBody()));
     }
   }
 }
--- java/Send.java
+++ java/Send.java
@@ -4,25 +4,16 @@ import com.rabbitmq.client.Channel;
 import java.io.IOException;
 
 public class Send {
-  public static void main(String[] argv) {
-    try {
-      Connection conn = null;
-      try {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        conn = factory.newConnection();
-        Channel chan = conn.createChannel();
-        chan.queueDeclare("hello", false, false, false, null);
-        chan.basicPublish("", "hello", null, "Hello World!".getBytes());
-        System.out.println(" [x] Sent 'Hello World!'");
-      }
-      finally {
-        if (conn != null) conn.close();
-      }
-    }
-    catch (Exception e) {
-      System.err.println("Exception while publishing");
-      e.printStackTrace();
-    }
+  public static void main(String[] argv)
+      throws java.io.IOException {
+    Connection conn = null;
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    conn = factory.newConnection();
+    Channel chan = conn.createChannel();
+    chan.queueDeclare("hello", false, false, false, null);
+    chan.basicPublish("", "hello", null, "Hello World!".getBytes());
+    System.out.println(" [x] Sent 'Hello World!'");
+    conn.close();
   }
 }
