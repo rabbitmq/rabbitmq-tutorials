8f4b1edb82e65998b99592c59ed3e7d62a358e3f
Ann <awitbrock@vmware.com>
2011-01-13 19:37:41 +0000

    connection, channel, explicit parameters

--- java/Worker.java
+++ java/Worker.java
@@ -5,33 +5,36 @@ import com.rabbitmq.client.QueueingConsumer;
 
 public class Worker {
     public static void main(String[] argv)
-        throws java.io.IOException,
-               java.lang.InterruptedException {
-    Connection conn = null;
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-    conn = factory.newConnection();
-    Channel chan = conn.createChannel();
+                                throws java.io.IOException,
+                                java.lang.InterruptedException {
+        Connection connection = null;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-    // make durable
-    chan.queueDeclare("task_queue", true, false, false, null);
-    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        boolean durable = true;
+        channel.queueDeclare("task_queue", durable, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-    // prefetch 1
-    chan.basicQos(1);
-    QueueingConsumer consumer = new QueueingConsumer(chan);
-    chan.basicConsume("task_queue", false, consumer);
+        int prefetchCount = 1;
+        channel.basicQos(prefetchCount);
 
-    while (true) {
-      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-      String body = new String(delivery.getBody());
-      System.out.println(" [x] Received " + body);
-      Thread.sleep(doDots(body));	// simulate action
-      System.out.println(" [x] Done");
-      // acknowledge
-      chan.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        boolean autoAck = false;
+        channel.basicConsume("task_queue", autoAck, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String body = new String(delivery.getBody());
+            System.out.println(" [x] Received " + body);
+            Thread.sleep(doDots(body)); // simulate action
+            System.out.println(" [x] Done");
+            // acknowledge
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        }
     }
-  }
+
 
 
   private static int doDots(String body){ 
