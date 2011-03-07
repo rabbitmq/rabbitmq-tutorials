8727243035a16b09fefecce9f5bd6f34b8276c50
Ann Witbrock <awitbrock@vmware.com>
2011-03-07 19:10:38 +0000

    ReceiveLog added

---
+++ java/ReceiveLog.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class ReceiveLog {
+
+  private static final String EXCHANGE_NAME = "logs";
+
+  public static void main(String[] argv)
+                      throws java.io.IOException,
+                      java.lang.InterruptedException {
+
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+    String queueName = channel.queueDeclare().getQueue();
+    channel.queueBind(queueName, EXCHANGE_NAME, "");
+    
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    QueueingConsumer consumer = new QueueingConsumer(channel);
+    channel.basicConsume(queueName, true, consumer);
+
+    while (true) {
+      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+      String message = new String(delivery.getBody());
+
+      System.out.println(" [x] Received '" + message + "'");   
+    }
+  }
+}
