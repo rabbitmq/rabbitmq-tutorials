d9fde2f383f48e4763e5988a77508adcdfe810da
Ann Witbrock <awitbrock@vmware.com>
2011-03-17 23:36:26 +0000

    new ReceiveLogsDirect.java

---
+++ java/ReceiveLogsDirect.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class ReceiveLogsDirect {
+
+  private static final String EXCHANGE_NAME = "direct_logs";
+
+  public static void main(String[] argv)
+                      throws java.io.IOException,
+                      java.lang.InterruptedException {
+
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+    String queueName = channel.queueDeclare().getQueue();
+    
+    if (argv.length < 1){
+      System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+      System.exit(1);
+    }
+    
+    for(String severity : argv){    
+      channel.queueBind(queueName, EXCHANGE_NAME, severity);
+    }
+    
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    QueueingConsumer consumer = new QueueingConsumer(channel);
+    channel.basicConsume(queueName, true, consumer);
+
+    while (true) {
+      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+      String message = new String(delivery.getBody());
+      String routingKey = delivery.getEnvelope().getRoutingKey();
+
+      System.out.println(" [x] Received '" + routingKey + "':'" + message + "'");   
+    }
+  }
+}
