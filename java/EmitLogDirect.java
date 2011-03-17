bb3d9e66ef0fca822d8f9b0623a2a0da3db7ef8f
Ann Witbrock <awitbrock@vmware.com>
2011-03-17 23:35:14 +0000

    New EmitLogDirect.java

---
+++ java/EmitLogDirect.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class EmitLogDirect {
+
+  private static final String EXCHANGE_NAME = "direct_logs";
+
+  public static void main(String[] argv) 
+                      throws java.io.IOException {
+
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+    String severity = getSeverity(argv);
+    String message = getMessage(argv);
+
+    channel.basicPublish( EXCHANGE_NAME, severity, 
+            null,
+            message.getBytes());
+    System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+  
+  private static String getSeverity(String[] strings){
+    if (strings.length < 1)
+    	    return "info";
+    return strings[0];
+  }
+
+  private static String getMessage(String[] strings){ 
+    if (strings.length < 2)
+    	    return "Hello World!";
+    return joinStrings(strings, " ", 1);
+  }
+  
+  private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+    int length = strings.length;
+    if (length == 0 ) return "";
+    if (length < startIndex ) return "";
+    StringBuilder words = new StringBuilder(strings[startIndex]);
+    for (int i = startIndex + 1; i < length; i++) {
+        words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
+  }
+}
