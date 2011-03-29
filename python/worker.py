22af53da7a1db5df7f1728cde42e035ef555bcbb
Marek Majkowski <majek04@gmail.com>
2011-03-29 14:27:35 +0100

    Python tutorials updated to pika 0.9.5

--- python/worker.py
+++ python/worker.py
@@ -15,7 +15,7 @@ def callback(ch, method, properties, body):
     print " [x] Done"
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1, prefetch_size=0L)
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
                       queue='task_queue')
 
