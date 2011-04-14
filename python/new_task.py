4f084556ea2568527df4cb833d512cb5b2d38db8
Ann Witbrock <awitbrock@vmware.com>
2011-04-14 15:27:11 +0100

    format publish

--- python/new_task.py
+++ python/new_task.py
@@ -9,7 +9,8 @@ channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(exchange='', routing_key='task_queue',
+channel.basic_publish(exchange='',
+                      routing_key='task_queue',
                       body=message,
                       properties=pika.BasicProperties(
                          delivery_mode = 2, # make message persistent
