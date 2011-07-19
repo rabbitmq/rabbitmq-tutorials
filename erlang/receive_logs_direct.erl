789bb96a4f570c957773a51e005e2a262e6d39f6
Jani Monoses <jani@ubuntu.com>
2011-07-19 18:41:43 +0300

    Code for section 4 of the tutorial.

---
+++ erlang/receive_logs_direct.erl
@@ -0,0 +1,37 @@
+#!/usr/bin/env escript
+%%! -pz ./amqp_client ./rabbit_common ./amqp_client/ebin ./rabbit_common/ebin
+
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+main(Argv) ->
+    {ok, Connection} =
+        amqp_connection:start(#amqp_params_network{host = "localhost"}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+
+    amqp_channel:call(Channel, #'exchange.declare'{exchange = <<"direct_logs">>,
+                                                   type = <<"direct">>}),
+
+    #'queue.declare_ok'{queue = Queue} =
+        amqp_channel:call(Channel, #'queue.declare'{exclusive = true}),
+
+    lists:foreach(fun(S) ->
+                    amqp_channel:call(Channel, #'queue.bind'{exchange = <<"direct_logs">>,
+                                                             routing_key = list_to_binary(S),
+                                                             queue = Queue})
+                  end, Argv),
+
+    io:format(" [*] Waiting for logs. To exit press CTRL+C~n"),
+
+    amqp_channel:subscribe(Channel, #'basic.consume'{queue = Queue,
+                                                     no_ack = true}, self()),
+    receive
+        #'basic.consume_ok'{} -> ok
+    end,
+    loop(Channel).
+
+loop(Channel) ->
+    receive
+        {#'basic.deliver'{routing_key=RoutingKey}, #amqp_msg{payload = Body}} ->
+            io:format(" [x] ~p:~p~n", [RoutingKey, Body]),
+            loop(Channel)
+    end.
