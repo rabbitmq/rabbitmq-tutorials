ba4d5ae97941161a429d2eb62d5d76408704da60
Jani Monoses <jani@ubuntu.com>
2011-07-20 11:58:12 +0300

    Code for section 5 of the tutorial.

---
+++ erlang/emit_log_topic.erl
@@ -0,0 +1,27 @@
+#!/usr/bin/env escript
+%%! -pz ./amqp_client ./rabbit_common ./amqp_client/ebin ./rabbit_common/ebin
+
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+main(Argv) ->
+    {ok, Connection} =
+        amqp_connection:start(#amqp_params_network{host = "localhost"}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+
+    amqp_channel:call(Channel, #'exchange.declare'{exchange = <<"topic_logs">>,
+                                                   type = <<"topic">>}),
+
+    {RoutingKey, Message} = case Argv of
+           [] -> {<<"anonymous.info">>, <<"Hello World!">>};
+           [R] -> {list_to_binary(R), <<"Hello World!">>};
+           [R | Msg] -> {list_to_binary(R), list_to_binary(string:join(Msg, " "))}
+          end,
+    amqp_channel:cast(Channel,
+                      #'basic.publish'{
+                          exchange = <<"topic_logs">>,
+                          routing_key = RoutingKey},
+                      #amqp_msg{payload = Message}),
+    io:format(" [x] Sent ~p:~p~n", [RoutingKey, Message]),
+    ok = amqp_channel:close(Channel),
+    ok = amqp_connection:close(Connection),
+    ok.
