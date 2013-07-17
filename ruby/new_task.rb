a1fca854ea166fb32e738814b5776263374a0dab
Michael Klishin <michael@defprotocol.org>
2013-07-17 21:54:43 +0400

    Tutorial 2 ported to Bunny

---
+++ ruby/new_task.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch   = conn.create_channel
+q    = ch.queue("task_queue", :durable => true)
+
+msg  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
+
+q.publish(msg, :persistent => true)
+puts " [x] Published #{msg}"
+
+sleep 1.0
+conn.close
---
+++ ruby/worker.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch   = conn.create_channel
+q    = ch.queue("task_queue", :durable => true)
+
+ch.prefetch(1)
+puts " [*] Waiting for messages. To exit press CTRL+C"
+
+begin
+  q.subscribe(:ack => true, :block => true) do |delivery_info, properties, body|
+    puts " [x] Received #{body}"
+    # imitate some work
+    sleep 1.0
+    puts " [x] Done"
+
+    ch.ack(delivery_info.delivery_tag)
+  end
+rescue Interrupt => _
+  puts " [*] Shutting down..."
+end
