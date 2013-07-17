6e2773c77491ac91ee776b61785142f948976ef4
Michael Klishin <michael@defprotocol.org>
2013-07-18 01:25:27 +0400

    Tutorial 5 is ported to Bunny

---
+++ ruby/receive_logs_topic.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+if ARGV.empty?
+  abort "Usage: #{$0} [binding key]"
+end
+
+conn = Bunny.new
+conn.start
+
+ch  = conn.create_channel
+x   = ch.topic("topic_logs")
+q   = ch.queue("", :exclusive => true)
+
+ARGV.each do |severity|
+  q.bind(x, :routing_key => severity)
+end
+
+puts " [*] Waiting for logs. To exit press CTRL+C"
+
+begin
+  q.subscribe(:block => true) do |delivery_info, properties, body|
+    puts " [x] #{delivery_info.routing_key}:#{body}"
+  end
+rescue Interrupt => _
+  puts " [*] Shutting down..."
+
+  ch.close
+  conn.close
+end
