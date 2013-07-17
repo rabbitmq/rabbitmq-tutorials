8b5971a1eb54b9d57c494ca2f52a02c706b09228
Michael Klishin <michael@defprotocol.org>
2013-07-17 23:11:23 +0400

    Tutorial 3 ported to Bunny

---
+++ ruby/emit_log.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch   = conn.create_channel
+x    = ch.fanout("logs")
+
+msg  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
+
+x.publish(msg)
+puts " [x] Sent '#{msg}'"
+
+sleep 0.5
+conn.close
---
+++ ruby/receive_logs.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch  = conn.create_channel
+x   = ch.fanout("logs")
+q   = ch.queue("", :exclusive => true)
+
+q.bind(x)
+
+puts " [*] Waiting for logs. To exit press CTRL+C"
+
+begin
+  q.subscribe(:block => true) do |delivery_info, properties, body|
+    puts " [x] #{body}"
+  end
+rescue Interrupt => _
+  puts " [*] Shutting down..."
+
+  ch.close
+  conn.close
+end
