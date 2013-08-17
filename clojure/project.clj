1e524f6fb0142ccf2721a11e148fb2572d8b0ac6
Michael Klishin <michael@defprotocol.org>
2013-08-17 20:48:06 +0400

    Use Langohr 1.4.x

--- clojure/project.clj
+++ clojure/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.5.1"]
-                 [com.novemberain/langohr "1.3.0"]])
+                 [com.novemberain/langohr "1.4.1"]])
