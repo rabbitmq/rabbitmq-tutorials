845a8cf851df446cefcd2b8f265a0afcf9f6bc11
Keith Elder <keith@keithelder.net>
2013-08-24 22:21:22 -0500

    Update ReceiveLogs.cs

--- dotnet/ReceiveLogs.cs
+++ dotnet/ReceiveLogs.cs
@@ -1,30 +1,35 @@
 using System;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Text;
 
-class ReceiveLogs {
-    public static void Main() {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.HostName = "localhost";
-        using (IConnection connection = factory.CreateConnection())
-        using (IModel channel = connection.CreateModel()) {
-            channel.ExchangeDeclare("logs", "fanout");
+class ReceiveLogs
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("logs", "fanout");
 
-            string queue_name = channel.QueueDeclare();
+                var queue_name = channel.QueueDeclare();
 
-            channel.QueueBind(queue_name, "logs", "");
-            QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
-            channel.BasicConsume(queue_name, true, consumer);
+                channel.QueueBind(queue_name, "logs", "");
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume(queue_name, true, consumer);
 
-            Console.WriteLine(" [*] Waiting for logs." +
-                              "To exit press CTRL+C");
-            while(true) {
-                BasicDeliverEventArgs ea =
-                    (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                Console.WriteLine(" [*] Waiting for logs." +
+                                  "To exit press CTRL+C");
+                while (true)
+                {
+                    var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
 
-                byte[] body = ea.Body;
-                string message = System.Text.Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] {0}", message);
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                }
             }
         }
     }
