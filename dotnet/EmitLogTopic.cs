97edb5285fe3de7d1b343aa292be3ca2a2e2b63e
Keith Elder <keith@keithelder.net>
2013-08-24 22:11:37 -0500

    Reformatted and cleaned up

--- dotnet/EmitLogTopic.cs
+++ dotnet/EmitLogTopic.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Linq;
 using RabbitMQ.Client;
+using System.Text;
 
-class EmitLogTopic {
-    public static void Main(string[] args) {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.HostName = "localhost";
-        using (IConnection connection = factory.CreateConnection())
-        using (IModel channel = connection.CreateModel()) {
+class EmitLogTopic
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
             channel.ExchangeDeclare("topic_logs", "topic");
 
-            string routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
-            string message = (args.Length > 1) ? string.Join(" ", args.Skip(1)
-                                                                  .ToArray())
-                                               : "Hello World!";
-            byte[] body = System.Text.Encoding.UTF8.GetBytes(message);
+            var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+            var message = (args.Length > 1) ? string.Join(" ", args.Skip(1)
+                                                                              .ToArray())
+                                                           : "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
             channel.BasicPublish("topic_logs", routingKey, null, body);
             Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
         }
