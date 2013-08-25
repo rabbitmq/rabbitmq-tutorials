5e94632260791f8058320372324acff16e94a9a7
Keith Elder <keith@keithelder.net>
2013-08-24 22:26:41 -0500

    Reformatted and cleaned up

--- dotnet/ReceiveLogsDirect.cs
+++ dotnet/ReceiveLogsDirect.cs
@@ -1,6 +1,7 @@
 using System;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Text;
 
 class ReceiveLogsDirect
 {
@@ -12,7 +13,7 @@ class ReceiveLogsDirect
             using (var channel = connection.CreateModel())
             {
                 channel.ExchangeDeclare("direct_logs", "direct");
-                var queue_name = channel.QueueDeclare();
+                var queueName = channel.QueueDeclare();
 
                 if (args.Length < 1)
                 {
@@ -22,23 +23,23 @@ class ReceiveLogsDirect
                     return;
                 }
 
-                foreach (string severity in args)
+                foreach (var severity in args)
                 {
-                    channel.QueueBind(queue_name, "direct_logs", severity);
+                    channel.QueueBind(queueName, "direct_logs", severity);
                 }
 
                 Console.WriteLine(" [*] Waiting for messages. " +
                                   "To exit press CTRL+C");
 
                 var consumer = new QueueingBasicConsumer(channel);
-                channel.BasicConsume(queue_name, true, consumer);
+                channel.BasicConsume(queueName, true, consumer);
 
                 while (true)
                 {
                     var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
 
                     var body = ea.Body;
-                    var message = System.Text.Encoding.UTF8.GetString(body);
+                    var message = Encoding.UTF8.GetString(body);
                     var routingKey = ea.RoutingKey;
                     Console.WriteLine(" [x] Received '{0}':'{1}'",
                                       routingKey, message);
