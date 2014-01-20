ac6c7e46da1d098939925a31b7ee18a3a34157cb
Michael Klishin <mklishin@gopivotal.com>
2014-01-20 15:56:35 +0400

    Extract fn

--- dotnet/EmitLog.cs
+++ dotnet/EmitLog.cs
@@ -12,11 +12,15 @@ class EmitLog
         {
             channel.ExchangeDeclare("logs", "fanout");
 
-            var message = (args.Length > 0) ? string.Join(" ", args)
-                                                           : "info: Hello World!";
+            var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
             channel.BasicPublish("logs", "", null, body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
+    }
+
+    private static string GetMessage(string[] args)
+    {
+      return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
     }
 }
