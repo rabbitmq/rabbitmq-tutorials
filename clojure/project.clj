5c85bb3033e6a55d2518a171ffcbf07ae8e16d79
Michael Klishin <mklishin@gopivotal.com>
2014-03-20 21:50:40 +0400

    Bump Langohr to 2.7.1

--- clojure/project.clj
+++ clojure/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.5.1"]
-                 [com.novemberain/langohr "2.3.2"]])
+                 [com.novemberain/langohr "2.7.1"]])
