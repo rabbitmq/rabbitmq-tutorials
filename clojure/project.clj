deb56f40ae418bfded8ee104fd5bb16bf24dbf08
Michael Klishin <mklishin@gopivotal.com>
2014-03-31 16:54:50 +0400

    Use Clojure 1.6.0

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/rabbitmq/rabbitmq-tutorial"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure     "1.5.1"]
+  :dependencies [[org.clojure/clojure     "1.6.0"]
                  [com.novemberain/langohr "2.7.1"]])
