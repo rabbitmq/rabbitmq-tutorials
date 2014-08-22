eb060defce7209c5e9b9ebce26dfa80e82a25e09
Michael Klishin <michael@clojurewerkz.org>
2014-08-22 17:33:50 +0400

    Bump Langohr to 3.0.0-rc1

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,7 +1,7 @@
 (defproject com.rabbitmq/tutorials "1.0.0-SNAPSHOT"
   :description "RabbitMQ tutorials using Langohr"
-  :url "http://github.com/rabbitmq/rabbitmq-tutorial"
+  :url "http://github.com/rabbitmq/rabbitmq-tutorials"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.6.0"]
-                 [com.novemberain/langohr "2.11.0"]])
+                 [com.novemberain/langohr "3.0.0-rc1"]])
