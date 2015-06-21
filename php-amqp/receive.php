d11638a2da0d3d8fc890f20adccdc7a69b22410f
Chimdi Azubuike <me@chimdi.com>
2015-06-20 21:25:40 -0700

    example 1 basic send receive

---
+++ php-amqp/receive.php
@@ -0,0 +1,49 @@
+#!/usr/bin/env php
+<?php
+/**
+	@author Chimdi Azubuike <me@chimdi.com>
+*/
+
+//Establish connection AMQP
+$connection = new AMQPConnection();
+$connection->setHost('127.0.0.1');
+$connection->setLogin('guest');
+$connection->setPassword('guest');
+$connection->connect();
+
+//Create and declare channel
+$channel = new AMQPChannel($connection);
+
+//AMQPC Exchange is the publishing mechanism
+$exchange = new AMQPExchange($channel);
+
+$routing_key = 'hello';
+$queue = new AMQPQueue($channel);
+$queue->setName($routing_key);
+$queue->setFlags(AMQP_NOPARAM);
+$queue->declareQueue();
+
+
+echo ' [*] Waiting for messages. To exit press CTRL+C ', PHP_EOL;
+
+$callback_func = function(AMQPEnvelope $message, AMQPQueue $q) use (&$max_consume) {
+	echo PHP_EOL, "------------", PHP_EOL;
+	echo " [x] Received ", $message->getBody(), PHP_EOL;
+	echo PHP_EOL, "------------", PHP_EOL;
+
+	$q->nack($message->getDeliveryTag());
+	sleep(1);
+};
+
+try{
+	$queue->consume($callback_func);
+}catch(AMQPQueueException $ex){
+	print_r($ex);
+}catch(Exception $ex){
+	print_r($ex);
+}
+
+echo 'Close connection...', PHP_EOL;
+$queue->cancel();
+$connection->disconnect();
+
---
+++ php-amqp/send.php
@@ -0,0 +1,35 @@
+#!/usr/bin/env php
+<?php
+/**
+	@author Chimdi Azubuike <me@chimdi.com>
+*/
+
+//Establish connection to AMQP
+$connection = new AMQPConnection();
+$connection->setHost('127.0.0.1');
+$connection->setLogin('guest');
+$connection->setPassword('guest');
+$connection->connect();
+
+//Create and declare channel
+$channel = new AMQPChannel($connection);
+
+
+//AMQPC Exchange is the publishing mechanism
+$exchange = new AMQPExchange($channel);
+
+
+try{
+	$routing_key = 'hello';
+
+	$queue = new AMQPQueue($channel);
+	$queue->setName($routing_key);
+	$queue->setFlags(AMQP_NOPARAM);
+	$queue->declareQueue();
+
+
+	$message = 'howdy-do';
+	$exchange->publish($message, $routing_key);
+}catch(Exception $ex){
+	print_r($ex);
+}
