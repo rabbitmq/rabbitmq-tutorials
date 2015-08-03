5b977da162fb403c15b4d5b5536c3d103dac50f7
Giuseppe Privitera <priviterag@gmail.com>
2015-08-03 17:11:24 +0100

    Publish/Subscribe tutorial receive_logs.js

---
+++ javascript-nodejs/src/receive_logs.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib');
+
+var conn = amqp.connect('amqp://localhost')
+
+conn.then(createChannel).then(null, console.warn);
+
+function createChannel(conn) {
+  process.once('SIGINT', function() { conn.close(); });
+  return conn.createChannel().then(function(ch) {
+    var ok = ch.assertExchange('logs', 'fanout', {durable: false});
+    ok = ok.then(function() {
+      return ch.assertQueue('', {exclusive: true});
+    });
+    ok = ok.then(function(qok) {
+      return ch.bindQueue(qok.queue, 'logs', '').then(function() {
+        return qok.queue;
+      });
+    });
+    ok = ok.then(function(queue) {
+      return ch.consume(queue, logMessage, {noAck: true});
+    });
+    return ok.then(function() {
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+    });
+
+    function logMessage(msg) {
+      console.log(" [x] '%s'", msg.content.toString());
+    }
+  });
+}
