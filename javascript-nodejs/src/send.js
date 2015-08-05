1143a0c1721281729f14a66f92490290b9422ccd
Giuseppe Privitera <priviterag@gmail.com>
2015-08-05 11:38:51 +0100

    declare transient queue

--- javascript-nodejs/src/send.js
+++ javascript-nodejs/src/send.js
@@ -6,7 +6,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'hello';
 
-    ch.assertQueue(q);
+    ch.assertQueue(q, {durable: false});
     ch.sendToQueue(q, new Buffer('Hello World!'));
     console.log(" [x] Sent 'Hello World!'");
   });
