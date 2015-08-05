b81868255c68f96668733076bc8a7b6c9fee3442
Giuseppe Privitera <priviterag@gmail.com>
2015-08-05 13:29:53 +0100

    refactored Work Queues tutorial

--- javascript-nodejs/src/new_task.js
+++ javascript-nodejs/src/new_task.js
@@ -1,28 +1,15 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
-var when = require('when');
+var amqp = require('amqplib/callback_api');
 
-var conn = amqp.connect('amqp://localhost');
-conn.then(createChannel).then(null, console.warn);
-
-function createChannel(conn) {
-  return when(
-    conn.createChannel().
-    then(sendMessage)).
-    ensure(function() {
-      conn.close();
-    });
-}
-
-function sendMessage(ch) {
-  var q = 'task_queue';
-  var ok = ch.assertQueue(q, {durable: true});
-
-  return ok.then(function(_ignore) {
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'task_queue';
     var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+    ch.assertQueue(q, {durable: true});
     ch.sendToQueue(q, new Buffer(msg), {persistent: true});
     console.log(" [x] Sent '%s'", msg);
-    return ch.close();
   });
-}
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
--- javascript-nodejs/src/worker.js
+++ javascript-nodejs/src/worker.js
@@ -1,34 +1,22 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
+var amqp = require('amqplib/callback_api');
 
-var conn = amqp.connect('amqp://localhost');
-conn.then(createChannel).then(null, console.warn);
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'task_queue';
 
-function createChannel(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel().then(consume);
-}
+    ch.assertQueue(q, {durable: true});
+    ch.prefetch(1);
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      var secs = msg.content.toString().split('.').length - 1;
 
-function consume(ch) {
-  var ok = ch.assertQueue('task_queue', {durable: true});
-
-  ok = ok.then(function() { ch.prefetch(1); });
-  ok = ok.then(function(_ignore) {
-    return ch.consume(
-      'task_queue',
-      function doSomeWork(msg) {
-        console.log(" [x] Received '%s'", msg.content.toString());
-        var secs = msg.content.toString().split('.').length - 1;
-        setTimeout(function() {
-          console.log(" [x] Done");
-          ch.ack(msg);
-        }, secs * 1000);
-      },
-      {noAck: false});
+      console.log(" [x] Received %s", msg.content.toString());
+      setTimeout(function() {
+        console.log(" [x] Done");
+        ch.ack(msg);
+      }, secs * 1000);
+    }, {noAck: false});
   });
-
-  return ok.then(function(_consumeOk) {
-    console.log(' [*] Waiting for messages. To exit press CTRL+C');
-  });
-}
+});
