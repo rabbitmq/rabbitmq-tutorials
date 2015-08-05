99a13e78d3b03a4db8fc62e7ded702b0747b8461
Giuseppe Privitera <priviterag@gmail.com>
2015-08-05 16:35:36 +0100

    refactored Publish/Subscribe tutorial code

--- javascript-nodejs/src/emit_log.js
+++ javascript-nodejs/src/emit_log.js
@@ -1,24 +1,16 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
-var when = require('when');
+var amqp = require('amqplib/callback_api');
 
-var conn = amqp.connect('amqp://localhost')
-conn.then(createChannel).then(null, console.warn);
-
-function createChannel(conn) {
-  return when(conn.createChannel().then(logMessage)).ensure(function() { conn.close(); });
-}
-
-function logMessage(ch) {
-  var ex = 'logs';
-  var ok = ch.assertExchange(ex, 'fanout', {durable: false})
-
-  return ok.then(function() {
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'logs';
     var msg = process.argv.slice(2).join(' ') || 'Hello World!';
 
+    ch.assertExchange(ex, 'fanout', {durable: false})
     ch.publish(ex, '', new Buffer(msg));
-    console.log(" [x] Sent '%s'", msg);
-    return ch.close();
+    console.log(" [x] Sent %s", msg);
   });
-}
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
--- javascript-nodejs/src/receive_logs.js
+++ javascript-nodejs/src/receive_logs.js
@@ -1,33 +1,20 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
-var conn = amqp.connect('amqp://localhost')
-var ch = conn.then(createChannel).then(null, console.warn);
+var amqp = require('amqplib/callback_api');
 
-ch.then(function(ch) {
-  var x = ch.assertExchange('logs', 'fanout', {durable: false});
-  var q = x.then(function() {
-    return ch.assertQueue('', {exclusive: true});
-  });
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'logs';
 
-  ok = q.then(function(qok) {
-    return ch.bindQueue(qok.queue, 'logs', '').then(function() {
-      return qok.queue;
+    ch.assertExchange(ex, 'fanout', {durable: false});
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      ch.bindQueue(q.queue, ex, '');
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [x] %s", msg.content.toString());
+      }, {noAck: true});
     });
   });
-  ok = ok.then(function(queue) {
-    return ch.consume(queue, logMessage, {noAck: true});
-  });
-  return ok.then(function() {
-    console.log(' [*] Waiting for logs. To exit press CTRL+C');
-  });
-
-  function logMessage(msg) {
-    console.log(" [x] '%s'", msg.content.toString());
-  }
 });
-
-function createChannel(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel();
-}
