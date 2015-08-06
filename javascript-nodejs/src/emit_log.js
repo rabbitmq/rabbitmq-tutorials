b98c82349250064456daa53689f51951331c8503
Giuseppe Privitera <priviterag@gmail.com>
2015-08-06 10:47:33 +0100

    missing semicolon

--- javascript-nodejs/src/emit_log.js
+++ javascript-nodejs/src/emit_log.js
@@ -7,7 +7,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
     var ex = 'logs';
     var msg = process.argv.slice(2).join(' ') || 'Hello World!';
 
-    ch.assertExchange(ex, 'fanout', {durable: false})
+    ch.assertExchange(ex, 'fanout', {durable: false});
     ch.publish(ex, '', new Buffer(msg));
     console.log(" [x] Sent %s", msg);
   });
