273e91a4c376eaba5985089e3bdea051bd6ec526
Giuseppe Privitera <priviterag@gmail.com>
2015-08-06 12:18:47 +0100

    refactored Routing tutorial

--- javascript-nodejs/src/emit_log_direct.js
+++ javascript-nodejs/src/emit_log_direct.js
@@ -1,26 +1,18 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
-var when = require('when');
+var amqp = require('amqplib/callback_api');
 
-var conn = amqp.connect('amqp://localhost')
-conn.then(createChannel).then(null, console.warn);
-
-function createChannel(conn) {
-  return when(conn.createChannel().then(logMessage)).ensure(function() { conn.close(); });
-}
-
-function logMessage(ch) {
-  var ex = 'direct_logs';
-  var ok = ch.assertExchange(ex, 'direct', {durable: false})
-
-  return ok.then(function() {
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'direct_logs';
     var args = process.argv.slice(2);
     var msg = args.slice(1).join(' ') || 'Hello World!';
     var severity = (args.length > 0) ? args[0] : 'info';
 
+    ch.assertExchange(ex, 'direct', {durable: false});
     ch.publish(ex, severity, new Buffer(msg));
-    console.log(" [x] Sent %s:'%s'", severity, msg);
-    return ch.close();
+    console.log(" [x] Sent %s: '%s'", severity, msg);
   });
-}
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
--- javascript-nodejs/src/receive_logs_direct.js
+++ javascript-nodejs/src/receive_logs_direct.js
@@ -1,46 +1,30 @@
 #!/usr/bin/env node
 
-usage();
+var amqp = require('amqplib/callback_api');
 
-var amqp = require('amqplib');
-var all = require('when').all;
-var severities = process.argv.slice(2);
-var conn = amqp.connect('amqp://localhost')
-var ch = conn.then(createChannel).then(null, console.warn);
+var args = process.argv.slice(2);
 
-ch.then(function(ch) {
-  var x = ch.assertExchange('direct_logs', 'direct', {durable: false});
-  var q = x.then(function() {
-    return ch.assertQueue('', {exclusive: true});
+if (args.length == 0) {
+  console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+  process.exit(1);
+}
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'direct_logs';
+
+    ch.assertExchange(ex, 'direct', {durable: false});
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+      args.map(function(severity) {
+        ch.bindQueue(q.queue, ex, severity);
+      });
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+      }, {noAck: true});
+    });
   });
-
-  var ok = q.then(function(qok) {
-    var queue = qok.queue;
-    return all(severities.map(function(sev) {
-      ch.bindQueue(queue, 'direct_logs', sev);
-    })).then(function() { return queue; });
-  });
-
-  ok = ok.then(function(queue) {
-    return ch.consume(queue, logMessage, {noAck: true});
-  });
-  return ok.then(function() {
-    console.log(' [*] Waiting for logs. To exit press CTRL+C');
-  });
-
-  function logMessage(msg) {
-    console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
-  }
 });
-
-function createChannel(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel();
-}
-
-function usage() {
-  if (process.argv.length < 3) {
-    console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
-    process.exit(1);
-  }
-}
