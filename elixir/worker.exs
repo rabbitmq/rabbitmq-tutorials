aa207592b2e655a355938ada83ed12b3ed2789d5
Jeff Weiss <jeff.a.weiss@gmail.com>
2015-12-12 22:32:31 -0800

    add Elixir source for tutorial 2

---
+++ elixir/worker.exs
@@ -0,0 +1,28 @@
+defmodule Worker do
+  def wait_for_messages(channel) do
+    receive do
+      {:basic_deliver, payload, meta} ->
+        IO.puts " [x] Received #{payload}"
+        payload
+        |> to_char_list
+        |> Enum.count(fn x -> x == ?. end)
+        |> Kernel.*(1000)
+        |> :timer.sleep
+        IO.puts " [x] Done."
+        AMQP.Basic.ack(channel, meta.delivery_tag)
+
+        wait_for_messages(channel)
+    end
+  end
+end
+
+{:ok, connection} = AMQP.Connection.open
+{:ok, channel} = AMQP.Channel.open(connection)
+
+AMQP.Queue.declare(channel, "task_queue", durable: true)
+AMQP.Basic.qos(channel, prefetch_count: 1)
+
+AMQP.Basic.consume(channel, "hello")
+IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
+
+Worker.wait_for_messages(channel)
