55374bd433d8a28f3bb9ba8df63ef99c4260d29a
Michael Klishin <michael@clojurewerkz.org>
2015-12-21 06:07:42 +0300

    Print later

--- python/receive.py
+++ python/receive.py
@@ -8,8 +8,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
@@ -17,4 +15,5 @@ channel.basic_consume(callback,
                       queue='hello',
                       no_ack=True)
 
+print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
