8048f89de7038b3f2bc3ecfd9577f445e9b5cabf
Andrew Bruce <me@andrewbruce.net>
2016-04-06 13:49:45 +0100

    Update objc tutorial1
    Connecting can now produce an error, so handle it.
    
    [finishes #116521563]

--- objective-c/tutorial1/tutorial1/ViewController.m
+++ objective-c/tutorial1/tutorial1/ViewController.m
@@ -14,41 +14,52 @@
 
 - (void)send {
     RMQConnection *conn = [RMQConnection new];
-    [conn start];
+
+    NSLog(@"Attempting to connect to local RabbitMQ broker");
 
     NSError *error = NULL;
+    [conn startWithError:&error];
+    if (error) {
+        NSLog(@"Couldn't connect to local RabbitMQ broker: %@", error);
+        exit(1);
+    }
+
     id<RMQChannel> ch = [conn createChannelWithError:&error];
     if (error) {
         NSLog(@"Error creating channel: %@", error);
         [conn close];
         exit(1);
-    } else {
-        RMQQueue *q = [ch queue:@"hello"];
-        [q publish:@"Hello World!"];
-        NSLog(@"Sent 'Hello World!'");
-        [conn close];
     }
+
+    RMQQueue *q = [ch queue:@"hello"];
+    [q publish:@"Hello World!"];
+    NSLog(@"Sent 'Hello World!'");
+    [conn close];
 }
 
 - (void)receive {
     RMQConnection *conn = [RMQConnection new];
-    [conn start];
-
     NSError *error = NULL;
+    [conn startWithError:&error];
+    if (error) {
+        NSLog(@"Couldn't connect to local RabbitMQ broker: %@", error);
+        exit(1);
+    }
+
     id<RMQChannel> ch = [conn createChannelWithError:&error];
     if (error) {
         NSLog(@"Error creating channel: %@", error);
         [conn close];
         exit(1);
-    } else {
-        RMQQueue *q = [ch queue:@"hello"];
-        NSLog(@"Waiting for messages.");
-        [q subscribe:^(id<RMQMessage> _Nonnull m) {
-            NSLog(@"Received %@", m.content);
-            [conn close];
-            exit(0);
-        }];
     }
+
+    RMQQueue *q = [ch queue:@"hello"];
+    NSLog(@"Waiting for messages.");
+    [q subscribe:^(id<RMQMessage> _Nonnull m) {
+        NSLog(@"Received %@", m.content);
+        [conn close];
+        exit(0);
+    }];
 }
 
 @end
