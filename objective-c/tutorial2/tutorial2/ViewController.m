68e9b446e4742f4181c20b35d87110799af2fc85
Andrew Bruce <me@andrewbruce.net>
2016-04-25 10:46:38 +0100

    Update objc tutorial2 for client changes
    [#116890463]

---
+++ objective-c/tutorial2/tutorial2/ViewController.m
@@ -0,0 +1,78 @@
+#import "ViewController.h"
+@import RMQClient;
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self workerNamed:@"Jack"];
+    [self workerNamed:@"Jill"];
+    sleep(1);
+    [self newTask:@"Hello World..."];
+    [self newTask:@"Just one this time."];
+    [self newTask:@"Five....."];
+    [self newTask:@"None"];
+    [self newTask:@"Two..dots"];
+}
+
+- (void)newTask:(NSString *)msg {
+    RMQConnection *conn = [[RMQConnection alloc] initWithDelegate:[RMQConnectionDelegateLogger new]];
+    [conn start];
+
+    id<RMQChannel> ch = [conn createChannel];
+
+    RMQQueue *q = [ch queue:@"task_queue" options:RMQQueueDeclareDurable];
+
+    [q publish:msg];
+    NSLog(@"Sent %@", msg);
+
+    [conn close];
+}
+
+- (void)workerNamed:(NSString *)name {
+    RMQConnection *conn = [[RMQConnection alloc] initWithDelegate:[RMQConnectionDelegateLogger new]];
+    [conn start];
+
+    id<RMQChannel> ch = [conn createChannel];
+
+    RMQQueue *q = [ch queue:@"task_queue" options:RMQQueueDeclareDurable];
+
+    [ch basicQos:@1 global:NO];
+    NSLog(@"%@: Waiting for messages", name);
+
+    [q subscribe:^(id<RMQMessage>  _Nonnull message) {
+        NSLog(@"%@: Received %@", name, message.content);
+        // imitate some work
+        unsigned int sleepTime = (unsigned int)[message.content componentsSeparatedByString:@"."].count - 1;
+        NSLog(@"%@: Sleeping for %u seconds", name, sleepTime);
+        sleep(sleepTime);
+    }];
+}
+
+- (void)workerWithManualAckNamed:(NSString *)name {
+    RMQConnection *conn = [[RMQConnection alloc] initWithDelegate:[RMQConnectionDelegateLogger new]];
+    [conn start];
+
+    id<RMQChannel> ch = [conn createChannel];
+
+    RMQQueue *q = [ch queue:@"task_queue" options:RMQQueueDeclareDurable];
+
+    [ch basicQos:@1 global:NO];
+    NSLog(@"%@: Waiting for messages", name);
+
+    RMQBasicConsumeOptions manualAck = RMQBasicConsumeNoOptions;
+    [q subscribe:manualAck handler:^(id<RMQMessage>  _Nonnull message) {
+        NSLog(@"%@: Received %@", name, message.content);
+        // imitate some work
+        unsigned int sleepTime = (unsigned int)[message.content componentsSeparatedByString:@"."].count - 1;
+        NSLog(@"%@: Sleeping for %u seconds", name, sleepTime);
+        sleep(sleepTime);
+
+        [ch ack:message.deliveryTag];
+    }];
+}
+
+@end
