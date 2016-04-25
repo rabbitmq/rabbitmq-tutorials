3be4fee8bccd093a0c3472b1101576f8837c0c0f
Andrew Bruce <me@andrewbruce.net>
2016-04-25 12:06:05 +0100

    Objc tut 3: new Xcode project
    [#117056895]

---
+++ objective-c/tutorial3/tutorial3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tutorial3
+//
+//  Created by Pivotal on 25/04/2016.
+//  Copyright © 2016 RabbitMQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objective-c/tutorial3/tutorial3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tutorial3
+//
+//  Created by Pivotal on 25/04/2016.
+//  Copyright © 2016 RabbitMQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
