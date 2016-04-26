1284a517fdb06cbb7cef715d684fa1460f3fb6af
Andrew Bruce <me@andrewbruce.net>
2016-04-26 10:43:34 +0100

    New Xcode project for Objc tutorial 5
    [#118375823]

---
+++ objective-c/tutorial5/tutorial5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tutorial5
+//
+//  Created by Pivotal on 26/04/2016.
+//  Copyright © 2016 RabbitMQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objective-c/tutorial5/tutorial5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tutorial5
+//
+//  Created by Pivotal on 26/04/2016.
+//  Copyright © 2016 RabbitMQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
