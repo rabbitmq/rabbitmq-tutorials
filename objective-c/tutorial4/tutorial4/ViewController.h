58745c6160dcd5a4dc52817a6b65f2f8f5ed0463
Andrew Bruce <me@andrewbruce.net>
2016-04-26 09:59:09 +0100

    Tutorial 4 controller for Objective-C
    [#118335067]

--- objective-c/tutorial4/tutorial4/ViewController.h
+++ objective-c/tutorial4/tutorial4/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  tutorial4
-//
-//  Created by Pivotal on 25/04/2016.
-//  Copyright © 2016 RabbitMQ. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- objective-c/tutorial4/tutorial4/ViewController.m
+++ objective-c/tutorial4/tutorial4/ViewController.m
@@ -1,12 +1,5 @@
-//
-//  ViewController.m
-//  tutorial4
-//
-//  Created by Pivotal on 25/04/2016.
-//  Copyright © 2016 RabbitMQ. All rights reserved.
-//
-
 #import "ViewController.h"
+@import RMQClient;
 
 @interface ViewController ()
 
@@ -16,12 +9,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self receiveLogsDirect];
+    sleep(1);
+    [self emitLogDirect:@"Hello World!" severity:@"info"];
+    [self emitLogDirect:@"Missile button pressed" severity:@"warning"];
+    [self emitLogDirect:@"Launch mechanism jammed" severity:@"error"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)receiveLogsDirect {
+    RMQConnection *conn = [[RMQConnection alloc] initWithDelegate:[RMQConnectionDelegateLogger new]];
+    [conn start];
+
+    id<RMQChannel> ch = [conn createChannel];
+    RMQExchange *x    = [ch direct:@"direct_logs"];
+    RMQQueue *q       = [ch queue:@"" options:RMQQueueDeclareExclusive];
+
+    NSArray *severities = @[@"error", @"warning", @"info"];
+    for (NSString *severity in severities) {
+        [q bind:x routingKey:severity];
+    }
+
+    NSLog(@"Waiting for logs.");
+
+    [q subscribe:^(RMQDeliveryInfo * _Nonnull deliveryInfo, RMQMessage * _Nonnull message) {
+        NSLog(@"%@:%@", deliveryInfo.routingKey, message.content);
+    }];
+}
+
+- (void)emitLogDirect:(NSString *)msg severity:(NSString *)severity {
+    RMQConnection *conn = [[RMQConnection alloc] initWithDelegate:[RMQConnectionDelegateLogger new]];
+    [conn start];
+
+    id<RMQChannel> ch = [conn createChannel];
+    RMQExchange *x    = [ch direct:@"direct_logs"];
+
+    [x publish:msg routingKey:severity];
+    NSLog(@"Sent '%@'", msg);
+
+    [conn close];
 }
 
 @end
