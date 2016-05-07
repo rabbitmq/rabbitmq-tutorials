bdb21793c2e2b0abe1f7c2a54399a0deae9204b7
Francisco Ortiz <Fortiz@MacBook-Pro-de-Francisco.local>
2016-05-07 13:42:27 +0200

    Minor change

--- javascript-nodejs/src/send.js
+++ javascript-nodejs/src/send.js
@@ -5,11 +5,11 @@ var amqp = require('amqplib/callback_api');
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'hello';
-    var buffer = new Buffer('Hello World!');
+    var msg = 'Hello World!';
 
     ch.assertQueue(q, {durable: false});
-    ch.sendToQueue(q, buffer);
-    console.log(" [x] Sent %s", buffer.toString());
+    ch.sendToQueue(q, new Buffer(msg));
+    console.log(" [x] Sent %s", msg);
   });
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
