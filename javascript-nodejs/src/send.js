c544f2354df32707b5a5b4d9f7255ebaaea6d67b
Francisco Ortiz <Fortiz@MacBook-Pro-de-Francisco.local>
2016-05-07 13:24:52 +0200

    Change buffer creation approach

--- javascript-nodejs/src/send.js
+++ javascript-nodejs/src/send.js
@@ -5,10 +5,11 @@ var amqp = require('amqplib/callback_api');
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'hello';
+    var buffer = new Buffer('Hello World!');
 
     ch.assertQueue(q, {durable: false});
-    ch.sendToQueue(q, new Buffer('Hello World!'));
-    console.log(" [x] Sent 'Hello World!'");
+    ch.sendToQueue(q, buffer);
+    console.log(" [x] Sent %s", buffer.toString());
   });
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
