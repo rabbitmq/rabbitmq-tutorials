d04a0934cfd818259e890b37ac02df95cf301c70
Brian Stajkowski <brian.stajkowski@rackspace.com>
2016-12-19 13:18:04 -0800

    Correct emit_log_topic.py
    Sys.argv will always be greater than 1, but in the instance
    where the severity is supplied it would be greather than 2.
    
    Closes rabbitmq/rabbitmq-tutorials#110

--- python/emit_log_topic.py
+++ python/emit_log_topic.py
@@ -9,7 +9,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='topic_logs',
                          exchange_type='topic')
 
-routing_key = sys.argv[1] if len(sys.argv) > 1 else 'anonymous.info'
+routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
 channel.basic_publish(exchange='topic_logs',
                       routing_key=routing_key,
