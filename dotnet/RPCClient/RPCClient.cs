c718cada1d1e289ccf7c9da72ad4fc40b9f5cf2d
darrylsk <darrylskeard@hotmail.com>
2017-09-15 16:24:08 -0300

    Add back the missing main method

--- dotnet/RPCClient/RPCClient.cs
+++ dotnet/RPCClient/RPCClient.cs
@@ -62,3 +62,18 @@ public class RpcClient
         connection.Close();
     }
 }
+
+public class Rpc
+{
+    public static void Main()
+    {
+        var rpcClient = new RpcClient();
+
+        Console.WriteLine(" [x] Requesting fib(30)");
+        var response = rpcClient.Call("30");
+
+        Console.WriteLine(" [.] Got '{0}'", response);
+        rpcClient.Close();
+    }
+}
+
