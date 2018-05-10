95643358225f9071f74a69b8bade71a5be039c25
Thorsten Hirsch <t.hirsch@web.de>
2018-05-10 16:13:02 +0200

    removed queue object from sender, because he doesn't use a queue - instead he uses an exchange

--- ruby/send.rb
+++ ruby/send.rb
@@ -5,9 +5,10 @@ connection = Bunny.new(automatically_recover: false)
 connection.start
 
 channel = connection.create_channel
-queue = channel.queue('hello')
 
-channel.default_exchange.publish('Hello World!', routing_key: queue.name)
+# the default_exchange will route the message to a queue with the same name
+# as the routing_key, so it will end in the receiver's hello queue
+channel.default_exchange.publish('Hello World!', routing_key: 'hello')
 puts " [x] Sent 'Hello World!'"
 
 connection.close
