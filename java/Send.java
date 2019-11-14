299ef29f5627b9dc815e72a05e1cd2cbe1294fe2
Michael Klishin <michael@clojurewerkz.org>
2019-11-14 19:16:27 +0300

    import java.nio.charset.StandardCharsets

--- java/Send.java
+++ java/Send.java
@@ -2,6 +2,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.nio.charset.StandardCharsets;
+
 public class Send {
 
     private final static String QUEUE_NAME = "hello";
