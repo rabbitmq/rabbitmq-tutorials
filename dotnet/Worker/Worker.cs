a1308e2670e1b3b84947515d0981180c5b4306c4
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2020-03-06 15:52:04 +0200

    Reverted commit 089cd3c0926f22bdc7f864e922125438676a52a5 because it breaks the build for me

--- dotnet/Worker/Worker.cs
+++ dotnet/Worker/Worker.cs
@@ -30,7 +30,7 @@ class Worker
 
                 Console.WriteLine(" [x] Done");
 
-                model.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
             channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
 
