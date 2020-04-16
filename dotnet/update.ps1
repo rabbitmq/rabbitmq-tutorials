d9d70e7da54782221de71b78a4011f37d81c92ed
Luke Bakken <lbakken@pivotal.io>
2020-04-16 16:35:29 -0700

    Update RabbitMQ.Client to version 6.0.0

--- dotnet/update.ps1
+++ dotnet/update.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 'Latest' -ErrorAction 'Stop' -Verbose
 
 Get-Childitem -Recurse '*.csproj' | ForEach-Object {
-    & dotnet add $_ package --version '6.0.0-rc.3' RabbitMQ.Client
+    & dotnet add $_ package --version '6.0.0' RabbitMQ.Client
 }
