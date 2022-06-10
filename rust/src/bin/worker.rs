9b51b98751cc52327b1a16876c20f6944de730f8
Michael Klishin <michael@clojurewerkz.org>
2022-06-11 01:11:55 +0400

    Update Rust tutorial 2 consumer

--- rust/src/bin/worker.rs
+++ rust/src/bin/worker.rs
@@ -1,6 +1,7 @@
-use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties};
+use futures::StreamExt;
 use std::thread;
 use std::time::Duration;
+use lapin::{Connection, ConnectionProperties, options::*, types::FieldTable};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,25 +17,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .await?;
 
-    let consumer = channel
+    let mut consumer = channel
         .basic_consume(
             "task_queue",
             "consumer",
             BasicConsumeOptions::default(),
             FieldTable::default(),
         )
-        .await?;
+        .await
+        .expect("basic_consume");
 
     println!(" [*] Waiting for messages. To exit press CTRL+C");
 
-    for delivery in consumer {
-        let (channel, delivery) = delivery?;
-        println!(" [x] Received {:?}", std::str::from_utf8(&delivery.data)?);
-        thread::sleep(Duration::from_secs(delivery.data.len() as u64));
-        println!(" [x] Done");
-        channel
-            .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
-            .await?;
+    while let Some(delivery) = consumer.next().await {
+        if let Ok(delivery) = delivery {
+            println!(" [x] Received {:?}", std::str::from_utf8(&delivery.data)?);
+            thread::sleep(Duration::from_secs(delivery.data.len() as u64));
+            println!(" [x] Done");
+            delivery.ack(BasicAckOptions::default())
+                .await
+                .expect("basic_ack");
+        }
     }
 
     Ok(())
