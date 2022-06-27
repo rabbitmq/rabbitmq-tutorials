fce59eb21de3d4ea786fe77ec4a20b4b4aef57e3
Jonas Barkå <jonas.barka@gmail.com>
2022-06-27 15:42:21 +0200

    Changed Send.cs formatting to match tutorial text.
    Changed formatting to exactly match tutorial text at this page:
    https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html

--- dotnet/Send/Send.cs
+++ dotnet/Send/Send.cs
@@ -10,12 +10,19 @@ class Send
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
 
             string message = "Hello World!";
             var body = Encoding.UTF8.GetBytes(message);
 
-            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
 
