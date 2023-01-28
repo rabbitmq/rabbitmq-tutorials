cc1ba39ab84d230a531eeaa836337c0b0e749865
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-01-28 20:26:30 +0100

    "" -> string.Empty, const string

--- dotnet-7/Send/Send.cs
+++ dotnet-7/Send/Send.cs
@@ -7,10 +7,10 @@ using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-string message = "Hello World!";
+const string message = "Hello World!";
 var body = Encoding.UTF8.GetBytes(message);
 
-channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: body);
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
