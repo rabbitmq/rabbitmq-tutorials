b13bca50eb02fd92be48017105fe3417eb1f498f
Gerardo Tatzati <gerardotatzati@gmail.com>
2023-07-27 09:40:05 -0400

    Update emit_log_topic.py

--- python/emit_log_topic.py
+++ python/emit_log_topic.py
@@ -12,5 +12,5 @@ routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
 channel.basic_publish(
     exchange='topic_logs', routing_key=routing_key, body=message)
-print(" [x] Sent %r:%r" % (routing_key, message))
+print(f" [x] Sent {routing_key}:{message}")
 connection.close()
