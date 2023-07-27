2d7ce86c40c966ee3ec89f68617acc922e75de2f
Gerardo Tatzati <gerardotatzati@gmail.com>
2023-07-27 09:40:03 -0400

    Update emit_log_direct.py

--- python/emit_log_direct.py
+++ python/emit_log_direct.py
@@ -12,5 +12,5 @@ severity = sys.argv[1] if len(sys.argv) > 2 else 'info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
 channel.basic_publish(
     exchange='direct_logs', routing_key=severity, body=message)
-print(" [x] Sent %r:%r" % (severity, message))
+print(f" [x] Sent {severity}:{message}")
 connection.close()
