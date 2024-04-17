d7c8f938e76d2895229c793debfff3cf7e508c7c
Gabriele Santomaggio <g.santomaggio@gmail.com>
2024-04-17 14:41:22 +0200

    dotnet stream tutorial hello world
    Signed-off-by: Gabriele Santomaggio <g.santomaggio@gmail.com>

---
+++ dotnet-stream/Send/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+
+await streamSystem.CreateStream(new StreamSpec("hello-stream")
+{
+    MaxLengthBytes = 5_000_000_000
+});
+
+var producer = await Producer.Create(new ProducerConfig(streamSystem, "hello-stream")
+{
+    ConfirmationHandler = async confirmation =>
+    {
+        switch (confirmation.Status)
+        {
+            case ConfirmationStatus.Confirmed:
+                foreach (var message in confirmation.Messages)
+                {
+                    Console.WriteLine(
+                        $"Stream: {confirmation.Stream}, " +
+                        $"message: {Encoding.UTF8.GetString(message.Data.Contents)} confirmed");
+                };
+                break;
+            default:
+                foreach (var message in confirmation.Messages)
+                {
+                    Console.WriteLine(
+                        $"Stream: {confirmation.Stream}, " +
+                        $"message: {Encoding.UTF8.GetString(message.Data.Contents)} not confirmed " +
+                        $"with status {confirmation.Status}");
+                };
+                break;
+        }
+        await Task.CompletedTask;
+    }
+});
+
+for (var i = 0; i < 100; i++)
+{
+    await producer.Send(new Message(Encoding.UTF8.GetBytes($"Hello, World - {i}")));
+}
+
+await Task.Delay(500);
+
+Console.WriteLine(" [x] Press any key to exit");
+Console.ReadKey();
+await producer.Close();
---
+++ dotnet_stream/dotnet-stream/SendHello/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+
+await streamSystem.CreateStream(new StreamSpec("hello-stream")
+{
+    MaxLengthBytes = 5_000_000_000
+});
+
+var producer = await Producer.Create(new ProducerConfig(streamSystem, "hello-stream")
+{
+    ConfirmationHandler = async confirmation =>
+    {
+        switch (confirmation.Status)
+        {
+            case ConfirmationStatus.Confirmed:
+                foreach (var message in confirmation.Messages)
+                {
+                    Console.WriteLine(
+                        $"Stream: {confirmation.Stream}, " +
+                        $"message: {Encoding.UTF8.GetString(message.Data.Contents)} confirmed");
+                };
+                break;
+            default:
+                foreach (var message in confirmation.Messages)
+                {
+                    Console.WriteLine(
+                        $"Stream: {confirmation.Stream}, " +
+                        $"message: {Encoding.UTF8.GetString(message.Data.Contents)} not confirmed " +
+                        $"with status {confirmation.Status}");
+                };
+                break;
+        }
+        await Task.CompletedTask;
+    }
+});
+
+
+for (var i = 0; i < 100; i++)
+{
+    await producer.Send(new Message(Encoding.UTF8.GetBytes($"Hello, World - {i}")));
+}
+
+await Task.Delay(500);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
