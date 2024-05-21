54b902ff334e85320c75e898f515a87baee46e50
hogimn <hogimn@gmail.com>
2024-05-21 21:00:09 +0900

    Update JAR file path in usage instructions
    Changed instances of 'java -jar rabbit-tutorials.jar' to
    'java -jar target/rabbitmq-tutorials.jar' to ensure users
    execute the correct JAR file from the appropriate directory.

--- spring-amqp/src/main/java/org/springframework/amqp/tutorials/RabbitAmqpTutorialsApplication.java
+++ spring-amqp/src/main/java/org/springframework/amqp/tutorials/RabbitAmqpTutorialsApplication.java
@@ -37,18 +37,18 @@ public class RabbitAmqpTutorialsApplication {
 		return args -> {
 			System.out.println("This app uses Spring Profiles to control its behavior.\n");
 			System.out.println("Options are: ");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,receiver");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=work-queues,receiver");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=work-queues,sender");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=pub-sub,receiver");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=pub-sub,sender");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=routing,receiver");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=routing,sender");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=topics,receiver");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=topics,sender");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=rpc,client");
-			System.out.println("java -jar rabbit-tutorials.jar --spring.profiles.active=rpc,server");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=hello-world,receiver");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=hello-world,sender");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=work-queues,receiver");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=work-queues,sender");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=pub-sub,receiver");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=pub-sub,sender");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=routing,receiver");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=routing,sender");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=topics,receiver");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=topics,sender");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=rpc,client");
+			System.out.println("java -jar target/rabbitmq-tutorials.jar --spring.profiles.active=rpc,server");
 		};
 	}
 
