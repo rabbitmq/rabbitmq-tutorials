a4227e420b2be70effcd364862f8835951fd2d5a
mooreca <cameron.moore-carter@fphcare.co.nz>
2024-07-30 15:57:54 +1200

    Fix Python Stream tutorial by using correct argument key values, implement asyncio.Runner to exit correctly on Keyboard Interrupt, close producer and consumer when closing program.

--- python-stream/receive.py
+++ python-stream/receive.py
@@ -1,5 +1,4 @@
 import asyncio
-import signal
 
 from rstream import (
     AMQPMessage,
@@ -15,30 +14,27 @@ STREAM_RETENTION = 5000000000
 
 
 async def receive():
-    consumer = Consumer(host="localhost", username="guest", password="guest")
-    await consumer.create_stream(
-        STREAM_NAME, exists_ok=True, arguments={"MaxLengthBytes": STREAM_RETENTION}
-    )
+    async with Consumer(host="localhost", username="test", password="test") as consumer:
+        await consumer.create_stream(
+            STREAM_NAME, exists_ok=True, arguments={"max-length-bytes": STREAM_RETENTION}
+        )
 
-    loop = asyncio.get_event_loop()
-    loop.add_signal_handler(
-        signal.SIGINT, lambda: asyncio.create_task(consumer.close())
-    )
+        async def on_message(msg: AMQPMessage, message_context: MessageContext):
+            stream = message_context.consumer.get_stream(message_context.subscriber_name)
+            print("Got message: {} from stream {}".format(msg, stream))
+        try:
+            print("Press control +C to close")
+            await consumer.start()
+            await consumer.subscribe(
+                stream=STREAM_NAME,
+                callback=on_message,
+                offset_specification=ConsumerOffsetSpecification(OffsetType.FIRST, None),
+            )
+            await consumer.run()
+        except (KeyboardInterrupt, asyncio.exceptions.CancelledError):
+            print("Closing Consumer...")
+            await consumer.close()
+            raise
 
-    async def on_message(msg: AMQPMessage, message_context: MessageContext):
-        stream = message_context.consumer.get_stream(message_context.subscriber_name)
-        print("Got message: {} from stream {}".format(msg, stream))
-
-    print("Press control +C to close")
-    await consumer.start()
-    await consumer.subscribe(
-        stream=STREAM_NAME,
-        callback=on_message,
-        offset_specification=ConsumerOffsetSpecification(OffsetType.FIRST, None),
-    )
-    await consumer.run()
-    # give time to the consumer task to close the consumer
-    await asyncio.sleep(1)
-
-
-asyncio.run(receive())
+with asyncio.Runner() as runner:
+    runner.run(receive())
--- python-stream/send.py
+++ python-stream/send.py
@@ -14,7 +14,7 @@ async def send():
         password="guest",
     ) as producer:
         await producer.create_stream(
-            STREAM_NAME, exists_ok=True, arguments={"MaxLengthBytes": STREAM_RETENTION}
+            STREAM_NAME, exists_ok=True, arguments={"max-length-bytes": STREAM_RETENTION}
         )
 
         await producer.send(stream=STREAM_NAME, message=b"Hello, World!")
@@ -22,6 +22,7 @@ async def send():
         print(" [x] Hello, World! message sent")
 
         input(" [x] Press Enter to close the producer  ...")
+        await producer.close()
 
-
-asyncio.run(send())
+with asyncio.Runner() as runner:
+    runner.run(send())
