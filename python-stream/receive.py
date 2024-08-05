32f589fcdf35247a8f7dfa823ddf2707e4f8a7eb
Cameron Moore-Carter <cameron.moore-carter@fphcare.co.nz>
2024-08-06 11:35:20 +1200

    Add back graceful termination of consumer
    Wrapped the consumer in a try except to  gracefully terminate the program.

--- python-stream/receive.py
+++ python-stream/receive.py
@@ -23,14 +23,18 @@ async def receive():
             stream = message_context.consumer.get_stream(message_context.subscriber_name)
             print("Got message: {} from stream {}".format(msg, stream))
 
-        print("Press control +C to close")
+        print("Press control + C to close")
         await consumer.start()
         await consumer.subscribe(
             stream=STREAM_NAME,
             callback=on_message,
             offset_specification=ConsumerOffsetSpecification(OffsetType.FIRST, None),
         )
-        await consumer.run()
+        try:
+            await consumer.run()
+        except (KeyboardInterrupt, asyncio.CancelledError):
+            print("Closing Consumer...")
+            return
 
 
 with asyncio.Runner() as runner:
