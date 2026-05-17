28172d32816f9ef51b79bc5dff1bc2ae9862989a
Amburi Roy <tready.aparna@gmail.com>
2026-05-17 20:15:42 +0200

    Add package-lock and update URL
    Co-Authored-By: Amburi Roy <1125584+amburi@users.noreply.github.com>

--- javascript-nodejs-pipeline/producer.js
+++ javascript-nodejs-pipeline/producer.js
@@ -3,7 +3,7 @@
 const amqp = require('amqplib');
 
 async function main() {
-    const connection = await amqp.connect('amqp://localhost');
+    const connection = await amqp.connect('amqp://rabbitmq');
     const channel = await connection.createChannel();
 
     const inputQueue = 'pipeline.input';
